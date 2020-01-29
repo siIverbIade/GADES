bef00fe1206a2b8164582b36070fff10da2de54b
Leandro Suzano <zairzao@yahoo.com.br>
2020-01-29 14:03:59 -0300

    Auditoria do modelo database habilitada

--- src/main/java/com/cpd/Gades.java
+++ src/main/java/com/cpd/Gades.java
@@ -3,9 +3,11 @@ package com.cpd;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.neo4j.annotation.EnableNeo4jAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableNeo4jAuditing
 @EnableScheduling
 @EntityScan(basePackages = {"com.cpd.entity.nodes","com.cpd.entity.arrows","com.cpd.entity.abstracts"})
 public class Gades {
