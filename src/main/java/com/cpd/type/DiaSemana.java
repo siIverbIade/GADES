e41a374c1e8d2685a4cca2c7d5938b74317d8605
Leandro Suzano <zairzao@yahoo.com.br>
2020-02-10 16:16:19 -0300

    Enum para dias da semana aprimorado

--- src/main/java/com/cpd/type/DiaSemana.java
+++ src/main/java/com/cpd/type/DiaSemana.java
@@ -1,6 +1,42 @@
 package com.cpd.type;
 
+import com.cpd.utils.TextUtils;
 
 public enum DiaSemana {
-	Segunda, Terca, Quarta, Quinta, Sexta, Sabado
+	SEG("Seg","Segunda", "Segunda-Feira"), TER("Ter","Terça", "Terça-Feira"),
+	QUA("Qua","Quarta", "Quarta-Feira"), QUI("Qui","Quinta", "Quinta-Feira"), 
+	SEX("Sex","Sexta", "Sexta-Feira"), SAB("Sáb","Sábado", "Sábado"), 
+	DOM("Dom","Domingo", "Domingo");
+	
+	private final String text_short;
+	private final String text;
+	private final String text_full;
+	
+	DiaSemana(String text_short,String text, String text_full) {
+		this.text_short = text_short;
+        this.text = text;
+        this.text_full = text_full;
+	}
+	
+	DiaSemana() {
+		this.text_short = "";
+		this.text="";
+		this.text_full = "";
+	}
+
+	public String FormatoReduzido() {
+		return (text_short == "") ? this.toString() : text_short;
+    }
+
+	public String FormatoSimples() {
+		return (text == "") ? this.toString() : text;
+    }
+	
+	public String FormatoCompleto() {
+		return (text_full == "") ? this.toString() : text_full;
+    }
+	
+	public String FormatoAbreviado() {
+		return TextUtils.firstLetterUp(this.toString());
+	}
 }
