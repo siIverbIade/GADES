487e74f07c7f9b17e4a2878941889a68530dcc15
Leandro Suzano <zairzao@yahoo.com.br>
2020-02-10 16:21:22 -0300

    repositório dos rótulos

---
+++ src/main/java/com/cpd/repository/RotuloRepository.java
@@ -0,0 +1,17 @@
+package com.cpd.repository;
+
+import org.springframework.data.neo4j.annotation.Query;
+import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+import com.cpd.entity.nodes.RotuloCalendario;
+import com.cpd.model.RotuloCalendarioModel;
+
+@Repository
+public interface RotuloRepository extends Neo4jRepository<RotuloCalendario, Long> {
+    public RotuloCalendario findByNome(String nome);
+
+    @Query("MATCH (c:CalendarioEscolar {anoLetivo: {0}}) WITH c UNWIND c.calendario as cal MATCH (r:RotuloCalendario) WHERE Id(r)=cal RETURN r.nome as nome, r.temAula as temAula, r.simbolo as simbolo, r.descricao as descricao, r.foreColor as foreColor, r.backColor as backColor")
+    public List<RotuloCalendarioModel> rotulosAnoLetivo(int anoLetivo);
+
+}
