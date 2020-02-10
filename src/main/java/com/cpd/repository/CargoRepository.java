a11af1670e8b1442c2f1332b74968efa9953b690
Leandro Suzano <zairzao@yahoo.com.br>
2020-02-10 16:20:33 -0300

    format

--- src/main/java/com/cpd/repository/CargoRepository.java
+++ src/main/java/com/cpd/repository/CargoRepository.java
@@ -2,7 +2,6 @@ package com.cpd.repository;
 
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.stereotype.Repository;
-
 import com.cpd.entity.nodes.Cargo;
 
 @Repository
