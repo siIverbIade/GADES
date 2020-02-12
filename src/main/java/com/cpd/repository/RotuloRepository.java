72d29e0960e6f09e949d1bb996dad1be9ce247ec
Leandro Suzano <zairzao@yahoo.com.br>
2020-02-12 16:30:48 -0300

    encontrar todos rótulos globais

--- src/main/java/com/cpd/repository/RotuloRepository.java
+++ src/main/java/com/cpd/repository/RotuloRepository.java
@@ -13,5 +13,7 @@ public interface RotuloRepository extends Neo4jRepository<RotuloCalendario, Long
 
     @Query("MATCH (c:CalendarioEscolar {anoLetivo: {0}}) WITH c UNWIND c.calendario as cal MATCH (r:RotuloCalendario) WHERE Id(r)=cal RETURN r.nome as nome, r.temAula as temAula, r.simbolo as simbolo, r.descricao as descricao, r.foreColor as foreColor, r.backColor as backColor")
     public List<RotuloCalendarioModel> rotulosAnoLetivo(int anoLetivo);
+    
+    public List<RotuloCalendario> findByGlobal(boolean global);
 
 }
