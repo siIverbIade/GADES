50204e7c068eccb3cfc4916c901f8421fe955c47
Leandro Suzano <zairzao@yahoo.com.br>
2020-02-14 13:35:49 -0300

    format

--- src/main/java/com/cpd/model/EstadoModel.java
+++ src/main/java/com/cpd/model/EstadoModel.java
@@ -1,12 +1,13 @@
 package com.cpd.model;
 
 import com.cpd.entity.nodes.Estado;
-
 import org.springframework.data.neo4j.annotation.QueryResult;
 import lombok.Data;
 
 @Data
-@QueryResult /* esta anotação permite que o repositório spring data retorne o objeto */
+@QueryResult /* esta anotação permite que o repositório spring data retorne o objeto
+    numa consulta cypher, bastando nomear os campos após o return. Ex.: Match (t:Book) Return t.name as nome, t.pages as paginas
+*/
 public class EstadoModel {
 
     private int cod;
