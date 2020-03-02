3363bade62774ec059d1bfbaeb4398a84f3ee51d
Leandro Suzano <zairzao@yahoo.com.br>
2020-03-02 11:40:00 -0300

    testes++

--- src/test/java/com/cpd/Teste.java
+++ src/test/java/com/cpd/Teste.java
@@ -1,24 +1,22 @@
 package com.cpd;
 
-import javax.script.*;
+import com.cpd.utils.DateUtils;
+import com.cpd.utils.Debug;
 
 public class Teste {
     public static void main(String[] args) {
-        try {
-            ScriptEngineManager manager = new ScriptEngineManager();
-            ScriptEngine engine = manager.getEngineByName("javascript");
+        Debug.Print(DateUtils.now().getMillis());
 
-            String JsFunction1 = "function data(Y, R) {return ('1/5/' + Y);};";
-
-                engine.eval(JsFunction1);
-            try {
-                Invocable inv = (Invocable) engine;
-                System.out.println("Retorno:" + inv.invokeFunction("data", 2020, 0));
-            } catch (NoSuchMethodException e) {
-                e.getMessage();
-            }
-        } catch (ScriptException e) {
-            e.getMessage();
-        }
+        /*
+         * try { ScriptEngineManager manager = new ScriptEngineManager(); ScriptEngine
+         * engine = manager.getEngineByName("javascript");
+         * 
+         * String JsFunction1 = "function data(Y, R) {return ('1/5/' + Y);};";
+         * 
+         * engine.eval(JsFunction1); try { Invocable inv = (Invocable) engine;
+         * System.out.println("Retorno:" + inv.invokeFunction("data", 2020, 0)); } catch
+         * (NoSuchMethodException e) { e.getMessage(); } } catch (ScriptException e) {
+         * e.getMessage(); }
+         */
     }
 }
