44cfb99798b081785c271ae6fd418a5dc1cf94d3
Leandro Suzano <zairzao@yahoo.com.br>
2020-03-02 11:30:11 -0300

    adicionado campo ultimaAtualizacao para lembrar a data da útima atualização agendada que foi bem sucedida

--- src/main/java/com/cpd/entity/nodes/Config.java
+++ src/main/java/com/cpd/entity/nodes/Config.java
@@ -1,8 +1,12 @@
 package com.cpd.entity.nodes;
 
+import java.util.Date;
+
 import org.neo4j.ogm.annotation.GeneratedValue;
 import org.neo4j.ogm.annotation.Id;
 import org.neo4j.ogm.annotation.NodeEntity;
+import org.neo4j.ogm.annotation.typeconversion.DateString;
+
 import lombok.Data;
 
 @NodeEntity
@@ -16,6 +20,9 @@ public class Config {
 	
 	private int anoLetivo;
 	
+	@DateString("dd/MM/YYYY")
+	private Date ultimaAtualizacao;
+
 	public void setConfig(Config conf) {
 		if (conf.firstRun != null) {
 			this.firstRun=conf.firstRun;
